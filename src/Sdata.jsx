0e6e090bf75f42eeeed4bd22182e1e41af47ef5e
Jenil Vora <85816884+jenilvora0408@users.noreply.github.com>
2022-06-13 20:40:34 +0530

    Completed making cards

---
+++ src/Sdata.jsx
@@ -0,0 +1,39 @@
+const Sdata = [
+    {
+        sname: "DARK",
+        imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+        description: "#1 Trending on Netflix, 110M+ views since third day of release"
+    },
+    {
+        sname: "extra curricular",
+        imgscr: "https://image.kpopmap.com/2020/03/extracurricular-netflix-poster-news-1.png",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+        description: "#2 Trending on Netflix, 87M+ views since fifth day of release"
+    },
+    {
+        sname: "Stranger Things",
+        imgscr: "https://mlpnk72yciwc.i.optimole.com/cqhiHLc.WqA8~2eefa/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2022/05/FSkCESUWYAAThYb.jpg",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+        description: "#3 Trending on Netflix, 80M+ views since second day of release"
+    },
+    {
+        sname: "The Vampire Diaries",
+        imgscr: "https://images.justwatch.com/poster/8589251/s592",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+        description: "#4 Trending on Netflix, 55M+ views since tenth day of release"
+    },
+    {
+        sname: "My First-2 Love",
+        imgscr: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+        description: "#1 Trending on Netflix, 45M+ views since one week of release"
+    }
+]
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Card from "./Cards";
+import Sdata from "./Sdata";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// console.log(Sdata[2]);
+
+// function ncard(val) {}
+
+ReactDOM.render(
+  <>
+    <h1 className="heading_style"> List of top 5 Netflix Series in 2022 </h1>
+    {Sdata.map((val) => {
+      return (
+        <Card
+          imgsrc={val.imgscr}
+          title={val.title}
+          sname={val.sname}
+          link={val.links}
+          describe={val.description}
+        />
+      );
+    })}
+    ;
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
